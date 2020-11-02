6562db719c5ece4f15bb91e6d02fca62fb15a3c4
Leif Högberg <leihog@gmail.com>
2020-11-02 17:42:26 +0100

    fixed the JS demo/doc to reflect backend changes

--- api/README.md
+++ api/README.md
@@ -6,6 +6,8 @@
 
 Authentication is required in order to gain access to private data, for this we use OAuth 2. OAuth 2 relies on separate standards for the access tokens - we currently use Bearer tokens which are the simplest and most widespread token type.
 
+See the [extension documentation](../extension/README.md) for how to aquire an access token.
+
 ### Including the access token in requests
 
 The access token must be added to the request header.
@@ -25,10 +27,10 @@ Example:
       -H "Accept: application/json" \
       https://api.flattr.com/rest/v2/subscription-status
 
-Fetching this endpoint will return an object with the authenticated user's subscription status. The returned JSON has 3 parts, paying, expires_at and payload.
+Fetching this endpoint will return an object with the authenticated user's subscription status. The returned JSON has 3 parts, isPaying, expiresAt and payload.
 
 * `isPaying`: a boolean value that is true when the user has an active subscription.
-* `expiresAt`: The time, as a unix timestamp, at which this payload will have to be renewed. The extension should make sure to update this payload shortly after the expire time.
+* `expiresAt`: The time, as a [unix timestamp](https://en.wikipedia.org/wiki/Unix_time), at which this payload will have to be renewed. The extension should make sure to update this payload shortly after the expire time.
 * `payload`: a cryptographically signed and encoded string intended to be shared with third parties. The [payload](../publisher-website/README.md) is described in detail in the [client documentation](../publisher-website/README.md).
 
 **Response**
--- publisher-website/demo/main.js
+++ publisher-website/demo/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const examplePublicKey = '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5Ey6dSJ0rKUJ0K/+kguEPSNHnH8FekBq5DC5U3pka6qoGkuRuzpVApp6bQ8Ks/ZTAH2yHoGe3SrP4HJo1JJ/KJH4+q3f0oW8zKXonpGAOHZ5SOasFHWj5rNdEwVsBT2mGMvKjVcx6m736KRSPfv4Ufx9eoFmll2AgG9Xmq3vUU/LWQSWgwZtJdBAE2k22JsI9S8eQYpAyg89d/81gM7W+WUR7xmAaTnigdV5Xj0NrPlUm/qh01NNY1gKbxiPdz54/vy5Govf8I9R/FBHewlqPHXFIaZsb2QZ+4LHk7LlEW+H47HU2krtamu+a182nwBSIJnlzDXi0nhul6H6pVzQcwIDAQAB-----END PUBLIC KEY-----'
-const examplePayload = 'eyJwYXlpbmciOnRydWUsInRzIjoxNjAzODczODMxfQ.p6V9Kt6nA8l7Bl656ZT4Y33OblczVkcufIQMAsDcLvBfsbvS5G26+OVA1J2Ltt7sKibiTkiY6WQL9m6/8awF+4aSL//fKc8Lh3kocJN4Hx0fBffH3PWtheCOPFpkFgndJF/Sk2lsscTMEp7mxXqVL3uTaHkbeUihL8c0miMRVbQ1zDyfyBQ611TNlvXaRQvy87OB1a9ytLSROv474crlTtDVVjVuW68keiIvJtGky5DVTGTn1F+Kac5ELBrWE1nUOmiLBVft5yAi40ZaOmSMiwyhFJRsb+VmPjncCjwBbEeHyw7Xx7q39/OPGSsVwNEAQ4K3XxDCTYFl380m+dDAcw'
+const examplePayload = 'eyJleHBpcmVzQXQiOjE2MDQ5MzUwMDIsImlzUGF5aW5nIjp0cnVlfQ.HWPa6JRnLFyB3J2/UwuPo0eAJg8Ncy1R/DRFlzpNa0FbdmsGMItRup9ONJmrGlZT5ByR6jGMljOP7SoC6cu073pXQPDSu6SOKPYprFLHbDZVf2T6m99AUnR9Db7NvtIhhzb2eRg9/QHfb6SaSIng0d5k+P9I18RVFCl7tAD4nw9Y4XMHOCWbXrcFuqqtWUtI663BVb98Asb8SIzbghBiuug6l9csiv8mAOYzwMpfFT/fjUa27aqBqroJsGv5gnqp/tVPZ2K5yWDd1Qc619ijg0tebTSw511i8zXRCBO6c3oOKp5ZkF8PuyIdU0Mbct9JjpRHgTXfU1+rh2g7fH96EQ'
 
 const algorithm = {
   name: 'RSASSA-PKCS1-v1_5',
@@ -17,16 +17,15 @@ const pemToArrayBuffer = function (pem) {
   // cleanKey should be a raw base64 string
   const cleanKey = pem.replace(/\n|-----BEGIN PUBLIC KEY-----|-----END PUBLIC KEY-----/g, '')
 
-  return keyToArrayBuffer(cleanKey);
+  return toArrayBuffer(window.atob(cleanKey));
 }
 
-const keyToArrayBuffer = function (rawKey) {
-  const decoded = window.atob(rawKey)
-  const buffer = new ArrayBuffer(decoded.length)
+const toArrayBuffer = function (data) {
+  const buffer = new ArrayBuffer(data.length)
   let bytes = new Uint8Array(buffer)
 
-  for (let i = 0; i < decoded.length; i++) {
-    bytes[i] = decoded.charCodeAt(i)
+  for (let i = 0; i < data.length; i++) {
+    bytes[i] = data.charCodeAt(i)
   }
 
   return bytes
@@ -46,8 +45,8 @@ const verifyPayload = async function (payload) {
   const isValid = await window.crypto.subtle.verify(
     algorithm,
     importedKey,
-    keyToArrayBuffer(signature),
-    keyToArrayBuffer(data)
+    toArrayBuffer(window.atob(signature)),
+    toArrayBuffer(data)
   )
 
   const parsedData = JSON.parse(window.atob(data))
