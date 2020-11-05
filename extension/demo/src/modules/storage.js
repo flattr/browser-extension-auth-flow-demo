5edf1b953686acb4dcdae630f623e391338643e6
Jon Englund <1138273+Karagath@users.noreply.github.com>
2020-11-05 15:27:37 +0100

    Update extension/demo/src/modules/storage.js
    Co-authored-by: Gabriel Burning <7988867+gburning@users.noreply.github.com>

--- extension/demo/src/modules/storage.js
+++ extension/demo/src/modules/storage.js
@@ -8,9 +8,7 @@ export async function set (data) {
   return !browser.runtime.lastError
 }
 
-export function get (key) {
-  return new Promise(async resolve => {
-    const results = await browser.storage.local.get([key])
-    resolve(results[key])
-  })
+export async function get (key) {
+  const results = await browser.storage.local.get([key])
+  return results[key]
 }
